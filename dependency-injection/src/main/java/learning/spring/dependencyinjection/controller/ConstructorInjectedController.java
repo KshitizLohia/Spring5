3ed799684ca7179f72f96a4407ba32517a929546
Kshitiz Lohia <kshitiz.lohia@inmobi.com>
2018-04-16 00:32:30 +0530

    Added serivce using Qualifier

--- dependency-injection/src/main/java/learning/spring/dependencyinjection/controller/ConstructorInjectedController.java
+++ dependency-injection/src/main/java/learning/spring/dependencyinjection/controller/ConstructorInjectedController.java
@@ -2,14 +2,14 @@ package learning.spring.dependencyinjection.controller;
 
 import learning.spring.dependencyinjection.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ConstructorInjectedController {
     private GreetingService greetingService;
 
-    @Autowired
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
--- dependency-injection/src/main/java/learning/spring/dependencyinjection/controller/SetterInjectedController.java
+++ dependency-injection/src/main/java/learning/spring/dependencyinjection/controller/SetterInjectedController.java
@@ -3,6 +3,7 @@ package learning.spring.dependencyinjection.controller;
 import learning.spring.dependencyinjection.service.GreetingService;
 import learning.spring.dependencyinjection.service.GreetingServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -10,6 +11,7 @@ public class SetterInjectedController {
     private GreetingService greetingService;
 
     @Autowired
+    @Qualifier("setterGreetingService")
     public void setGreetingService(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
