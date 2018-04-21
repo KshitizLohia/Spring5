a7f8e9b4c815f0ef729caf8f4a014c5e4f9e1454
Kshitiz Lohia <kshitiz.lohia@inmobi.com>
2018-04-21 22:28:01 +0530

    ENV Property config

--- spring5-factory-beans/src/main/java/guru/springframework/config/PropertyConfig.java
+++ spring5-factory-beans/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,16 +1,26 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDatasource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 import jdk.nashorn.internal.objects.annotations.Property;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
+
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySource({
+        "classpath:datasource.properties",
+        "classpath:jms.properties"
+})
 public class PropertyConfig {
+
+    @Autowired
+    Environment environment;
 
     @Value("${guru.username}")
     private String user;
@@ -21,9 +31,18 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     private String dburl;
 
+    @Value("${guru.jms.username}")
+    private String jmsUser;
+
+    @Value("${guru.jms.password}")
+    private String jmsPassword;
+
+    @Value("${guru.jms.dburl}")
+    private String jmsDburl;
+
 
     @Bean
-    public FakeDatasource fakeDatasource(){
+    public FakeDatasource fakeDatasource() {
         FakeDatasource fakeDatasource = new FakeDatasource();
         fakeDatasource.setDburl(dburl);
         fakeDatasource.setPassword(password);
@@ -32,7 +51,16 @@ public class PropertyConfig {
     }
 
     @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
+    public FakeJmsBroker fakeJmsBroker() {
+        FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+        fakeJmsBroker.setDburl(jmsDburl);
+        fakeJmsBroker.setPassword(jmsPassword);
+        fakeJmsBroker.setUser(jmsUser);
+        return fakeJmsBroker;
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
         return new PropertySourcesPlaceholderConfigurer();
 
     }
